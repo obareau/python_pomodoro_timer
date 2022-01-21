9a49f2a91461d7f0815f31ca667946f21d33c045
obareau <olivier.bareau@gmail.com>
2022-01-21 22:27:46 +0100

    gui v1

--- pomodoro.py
+++ pomodoro.py
@@ -1,4 +1,5 @@
 # A simple pomodoro timer
+from ast import Pass
 import time
 import threading
 import tkinter as tk
@@ -10,6 +11,35 @@ class PomodoroTimer:
         self.root.geometry("600x300")
         self.root.title("Olivier' Pomodoro Timer")
         self.root.tk.call('wm', 'iconphoto', self.root._w, PhotoImage(file="pomodoro.png"))
+        
+        self.s = ttk.Style()
+        self.s.configure("TNotebook.Tab", font=("Ubuntu", 16))
+        self.s.configure("TButton", font=("Ubuntu", 16))
+        
+        self.tabs = ttk.Notebook(self.root)
+        self.tabs.pack(fill="both", pady=10, expand=True)
+        
+        self.tab1 = ttk.Frame(self.tabs, width=600, height=100)
+        self.tab2 = ttk.Frame(self.tabs, width=600, height=100)
+        self.tab3 = ttk.Frame(self.tabs, width=600, height=100)
+        
+        self.tabs.add(self.tab1, text="Pomodoro")
+        self.tabs.add(self.tab2, text="Short Break")
+        self.tabs.add(self.tab3, text="Long Break")
+        
         self.root.mainloop()
+
+    def start_timer_thread(self):
+        pass
+    
+    def start_timer(self):
+        pass
+    
+    def reset_clock(self):
+        pass
+    
+    def skip_clock(self):
+        pass
+    
 
 PomodoroTimer()
